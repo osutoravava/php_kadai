74d65e6f7a0b71b917cd1f5438d536b3b882d3e4
osutoravava <osutoravava@gmail.com>
2022-06-24 15:12:08 +0900

    helloworld100回表示に改行を追加

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
-    echo "helloworld";
+//while制御文で試し打ち
+// $count = 1;
+
+// while ($count <= 100) {
+//   echo "helloworld{$count}<br />\n";
+//   $count++;
+// }
+  echo str_repeat("helloworld<br/>",100);
+
+
+
 ?>
