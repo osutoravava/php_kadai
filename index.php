f79993e0bb891cc9bbd3348217a75c907b28bcfa
osutoravava <osutoravava@gmail.com>
2022-06-24 16:39:37 +0900

    3の倍数の時だけWealthy Designを表示

--- index.php
+++ index.php
@@ -1,12 +1,34 @@
 <?php
 //while制御文で試し打ち
 // $count = 1;
-
 // while ($count <= 100) {
-//   echo "helloworld{$count}<br />\n";
-//   $count++;
+//   foreach($count as $num){
+
+
+//     if($num % 3 !== 0){
+//       echo "{$count}<br />\n";
+//       $count++;
+
+//     }
+
+  // echo "{$count}<br />\n";
+  // $count++;
+//   // }
+//   }
 // }
-  echo str_repeat("helloworld<br/>",100);
+  // echo str_repeat("helloworld<br/>",100);
+
+
+  $list = (range(1,100));
+
+  foreach($list as $num) {
+
+    if($num % 3 !== 0) {
+        continue;
+    }
+
+    echo $num . 'Wealthy Design<br>';
+  }
 
 
 
