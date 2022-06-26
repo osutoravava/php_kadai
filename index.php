5a99c7a04a8cae394b868d9d16a1861c59108f13
osutoravava <osutoravava@gmail.com>
2022-06-26 15:49:23 +0900

    index.phpの今までの部分をコメントアウトし、入力フォームを追加 出力用にresult.phpを追加

--- index.php
+++ index.php
@@ -1,11 +1,34 @@
-<?php
-  for($i = 1; $i < 100; $i++){
-    if($i%3 ==0 )  echo "Wealthy Design";
-    if($i%5 ==0)  echo "Wealthy Design";
-    echo $i ;
-    echo "<br>";
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP課題</title>
 
-  }
+</head>
+<body>
+<form action="result.php" method="get">
+ 名前: <input type="text" name="name" />
+ メールアドレス: <input type="email" name="mail" />
+ <input type="submit" />
+</form>
+
+</body>
+</html>
+
+<?php
+  //課題:3と5の倍数の時にWealthy Designと表示
+  // for($i = 1; $i < 100; $i++){
+  //   if($i%3 ===0 )  echo "Wealthy Design";
+  //   if($i%5 ===0)  echo "Wealthy Design";
+  //   echo $i ;
+  //   echo "<br>";
+
+  // }
+
+
+
 
 
 
---
+++ result.php
@@ -0,0 +1,2 @@
+こんにちは、<?php echo htmlspecialchars($_GET['name']); ?>さん。
+あなたのメールアドレスは、<?php echo $_GET['mail']; ?> です。
